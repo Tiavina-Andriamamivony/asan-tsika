c8ae39ab9a4725186acf584f0a2a3265432ca3af
Lou Andria <lou@hei.school>
2025-02-27 11:24:56 +0100

    fixup: format

--- src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
+++ src/main/java/school/hei/asa/endpoint/rest/controller/MissionController.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
